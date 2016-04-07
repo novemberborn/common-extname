dffe0d2a8b702846f66e8f7c837b209c217109f7
Mark Wubben <mark@novemberborn.net>
2016-04-07 15:09:36 +0100

    use as-i-preach for linting

--- test.js
+++ test.js
@@ -2,19 +2,19 @@ import test from 'ava'
 
 import commonExtname from './'
 
-test('returns an empty string if there is no common extname', (t) => {
+test('returns an empty string if there is no common extname', t => {
   t.is(commonExtname(['foo.bar', 'baz.qux']), '')
 })
 
-test('returns the longest common extname', (t) => {
+test('returns the longest common extname', t => {
   t.is(commonExtname(['foo.bar.baz', 'qux.quux.baz']), '.baz')
   t.is(commonExtname(['foo.bar.baz', 'qux.bar.baz']), '.bar.baz')
 })
 
-test('does not care about the file separator', (t) => {
+test('does not care about the file separator', t => {
   t.is(commonExtname(['foo/bar.baz', 'qux\\bar.baz']), '.baz')
 })
 
-test('works fine with surrogate pairs', (t) => {
+test('works fine with surrogate pairs', t => {
   t.is(commonExtname(['foo.bar.💩', 'qux.bar.💩']), '.bar.💩')
 })
